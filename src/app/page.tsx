393327897e6e51053a01f9ef7c8b1d85c872ebd7
Advsum <sumekah33@gmail.com>
2024-03-11 00:36:45 +0700

    adding the Descriptions for starting poing of next app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>This Is testing Proposes</h1>
+      <div className="container-of-of-apps">
+        <h1>
+          This is the Next App for Starting Point, All of my requrement of
+          developt will be use this App to initialize
+        </h1>
+      </div>
     </main>
   );
 }
