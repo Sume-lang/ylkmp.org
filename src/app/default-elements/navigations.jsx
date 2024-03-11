7a65b6210edd481d431b8db7643c419097c3dea9
Advsum <sumekah33@gmail.com>
2024-03-11 10:23:56 +0700

    adding the styles in navigations

--- src/app/default-elements/navigations.jsx
+++ src/app/default-elements/navigations.jsx
@@ -1,29 +1,130 @@
 import "./nav-bod-foo.css";
 function Navigations() {
   return (
-    <div className="navbar bg-base-100 color-white">
-      <div className="flex-1">
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a>About Us</a>
+              <ul className="p-2">
+                <li>
+                  <a>Our Profiles</a>
+                </li>
+                <li>
+                  <a>Our Mission and Mission</a>
+                </li>
+                <li>
+                  <a>Principle</a>
+                </li>
+                <li>
+                  <a>The Team Members</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Programe</a>
+              <ul className="p-2">
+                <li>
+                  <a>Food Security</a>
+                </li>
+                <li>
+                  <a>Rural Community</a>
+                </li>
+                <li>
+                  <a>Community Developtment</a>
+                </li>
+                <li>
+                  <a>Emergency Response</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Projects</a>
+              <ul className="p-2">
+                <li>
+                  <a>Current Project</a>
+                </li>
+                <li>
+                  <a>Strategy Plan 2024-2030</a>
+                </li>
+                <li>
+                  <a>Achievments</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Learning Platform</a>
+              <ul className="p-2">
+                <li>
+                  <a>Food Security Moduls</a>
+                </li>
+                <li>
+                  <a>Analitical Task</a>
+                </li>
+                <li>
+                  <a>Modul: Planning for Community Developtment</a>
+                </li>
+                <li>
+                  <a>Potential Resources</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>News Event</a>
+            </li>
+            <li>
+              <a>C-Community</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
-      <div className="flex-none">
+      <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <a>Item 1</a>
           </li>
           <li>
             <details>
               <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
+              <ul className="p-2">
                 <li>
-                  <a>Link 1</a>
+                  <a>Submenu 1</a>
                 </li>
                 <li>
-                  <a>Link 2</a>
+                  <a>Submenu 2</a>
                 </li>
               </ul>
             </details>
           </li>
+          <li>
+            <a>Item 3</a>
+          </li>
         </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
       </div>
     </div>
   );
